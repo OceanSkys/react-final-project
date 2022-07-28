f06db96dcd7523ef2dc4e076d8ebd246bc06916a
OceanSkys <90655578+OceanSkys@users.noreply.github.com>
2022-07-28 14:12:05 -0700

    Adjusted the way the products are displayed on screen, giving them flex-wrapand flex direction row. Added a moving animation I will be using for the home page later.

--- ecommercefinal/src/components/HeaderFooter.js
+++ ecommercefinal/src/components/HeaderFooter.js
@@ -3,15 +3,21 @@ import { Outlet, NavLink } from 'react-router-dom'
 
   export default function HeaderFooter() {
 
+    function styleByActiveStatus(isActive) {
+      return isActive
+        ? { fontWeight: "bold", textDecoration: "none" }
+        : { fontWeight: "normal", textDecoration: "none" };
+    }
+
     return (
       <div>
         <div className="HeaderFooterDiv">
           <div className="Logo">Coolest Store Ever</div>
             <div className="Navs">
               {/* Nav Links that take you to different page */}
-              <NavLink to='/'>Home</NavLink>
-              <NavLink to='/ProductDetails'>Prod-Details</NavLink>
-              <NavLink to='/Products'>Products</NavLink>
+              <NavLink to='/' style={({ isActive }) => styleByActiveStatus(isActive)}>Home</NavLink>
+              <NavLink to='/ProductDetails' style={({ isActive }) => styleByActiveStatus(isActive)}>Prod-Details</NavLink>
+              <NavLink to='/Products' style={({ isActive }) => styleByActiveStatus(isActive)}>Products</NavLink>
             </div>
         </div>
           <div style={{ padding: 40 }}>
--- ecommercefinal/src/components/ProductDetails.js
+++ ecommercefinal/src/components/ProductDetails.js
@@ -1,15 +1,76 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
-function ProductDetails() {
+export default function ProductsPicker() {
+    let [searchValue, setSearchValue] = useState('');
+    const [data, setData] = useState([]);
+    const [products, setProducts] = useState([]);
 
-    return (
-      <div className="ProductDetails">
-        <h1>This is the products details page</h1>
-        <Link to='/'>
-            <button>Take me home!</button>       
-        </Link>
+    useEffect(() =>{
+      getProducts()
+      console.log('this is products', products);
+    }, [])
+    
+    const getProducts = async () => {
+        
+        const url = `https://fakestoreapi.com/products/`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data)
+        console.log('this is products', products);
+
+      };
+
+var elements = document.querySelectorAll(".mover");
+for (var i = 0, len = elements.length; i < len; i++) {
+    (function (el) {
+        function reset() {
+            el.style.display = "none"; // make it immune to transitions
+            el.style.marginLeft = (el.parentElement.clientWidth) + "px"; //move it back
+            el.style.display = "inline-block"; // make it susceptible to transitions
+            moveAway();
+        }
+        function moveAway() {
+            el.style.marginLeft = (el.clientWidth * -1) + "px"; // transit occurs
+            setTimeout(reset, 4000); // 4 seconds later, start over
+        }
+        reset();
+    })(elements[i]);
+  }
+
+
+  // useEffect(() => {
+  //   console.log(products);
+  // }, [products]);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="Store">Jeremy's Store</h1>
+        {/* <h2>{products['1']?.title}</h2> */}
+          {products?.length > 0 ? (products?.map((prods, i) => (
+          <div key={i} className='Product-Selection'>
+            <div className="Product-Image-Div">
+              <img src={prods.image} className='Product-Image' alt="Item" width="300" height="400"/>
+            </div>
+            <div className='Product-Desc-Title'>
+            <div style={{width: '100%', height: '50%'}}>
+                <h1 className='Product-Title'>{prods.title}</h1>
+              </div>
+              <div style={{width: '100%', height: '50%', border: 'solid', marginBottom: '10px'}}>
+                <p className='Product-Description'>{prods.description}</p>
+              </div>
+            </div>
+          </div>          
+          ))) : (
+            <h1>Loading</h1>
+          )}
+          <div style={{width:'100%', border:'1px solid black'}}>
+            <div className="mover">inline</div>
+          </div>
+          <div className="mover" style={{position:'absolute'}}>absolute</div>
+        <NavLink to="/productdetails" className="btn btn-primary col-md-2 fw-bold justify-content-end  mb-5" type="submit">Let's Shop!</NavLink>
       </div>
+    </div>
     );
   }
-  
-  export default ProductDetails;
